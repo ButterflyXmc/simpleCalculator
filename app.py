9936884fae4584edd3d030f01998cb123555f4d8
mahic <mahic@domain.com>
2023-01-27 21:35:29 -0700

    Finished calc

--- app.py
+++ app.py
@@ -14,35 +14,25 @@ print("4: Divide")
 #! ...................................
 
 # user's selection
-# selection = input("Please enter your selection :")
-
+selection = input("Please enter your selection :")
 
 #user's first number 
-
+first = int(input("Enter the first number :"))
 
 #user's second number
+second = int(input("Enter the second number :"))
 
-
-#! Handling the simple error -> if the user types any other character 
-#! other than a number, user will get an error and will give them a chance to type a numer.
-while True:
-    selection = input("Please enter your selection :")
-    if selection in ("1", "2", "3", "4"):
-        first = int(input("Enter the first number"))
-        second = int(input("Enter the second number"))
-
-    if selection == "1":
-        print("Your answer is :")
-        print(add.addition(first,second))
-    elif selection == "2":
-        print("Your answer is :")
-        print(subtract.subtract(first,second))
-    elif selection == "3":
-        print("Your answer is :")
-        print(multiply.multiply(first,second))
-    elif selection == "4":
-        print("Your answer is :")
-        print(divide.division(first,second))
-        break
-    else:
-        print("Please enter a valid operation!")
+if selection == "1":
+    print("Your answer is :")
+    print(add.addition(first,second))
+elif selection == "2":
+    print("Your answer is :")
+    print(subtract.subtract(first,second))
+elif selection == "3":
+    print("Your answer is :")
+    print(multiply.multiply(first,second))
+elif selection == "4":
+    print("Your answer is :")
+    print(divide.division(first,second))
+else:
+    print("Please enter a valid operation!")
